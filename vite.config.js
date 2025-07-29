1810aefee831086e437859f676b12a413278b175
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-07-29 23:02:30 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit()
-	]
+        plugins: [
+                tailwindcss(),
+                sveltekit()
+        ],
+        preview: {
+    port: 4000,
+    host: true,
+    allowedHosts: ['udbsmartfarming.com', 'www.udbsmartfarming.com']
+  }
 
 });
